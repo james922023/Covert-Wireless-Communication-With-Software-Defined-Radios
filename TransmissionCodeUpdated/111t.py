c5010dbb25d0e75e8ad1ae48c6221fd95792a0f2
james922023 <jyackanich2022@fau.edu>
2024-11-01 20:39:10 -0400

    update 111t to work looped with small arrays

--- TransmissionCodeUpdated/111t.py
+++ TransmissionCodeUpdated/111t.py
@@ -8,7 +8,7 @@ from PIL import Image
 #SET PARAMETERS FOR THE RADIO
 sample_rate = 1000000 # Hz
 center_freq = 915e6 # Hz
-num_samps = 62 # number of samples per call to rx()
+num_samps = 52 # number of samples per call to rx()
 ack_freq = 900e6 # Hz
 
 sdr = adi.Pluto("ip:192.168.2.1")
@@ -40,7 +40,7 @@ start_sequence = np.array([1,1,1,-1,-1,-1,1,-1,-1,1,-1])
 
 #CREATE ARRAY OR USE IMAGE ARRAY AS STARTING POINT
 x_int = np.array([0,1,1,0,1,0,0,0,0,1,1,0,0,1,0,1,0,1,1,0,1,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0,1,1,1,1])  # 0 to 1 (binary)
-print("original bits (1 will be -1 and 0 will be 1): ", x_int)
+#print("original bits (1 will be -1 and 0 will be 1): ", x_int)
 
 # Define phase for BPSK: 0 for 0, π for 1
 x_radians = x_int * np.pi  # 0 for 0, π for 1
@@ -50,7 +50,7 @@ x_symbols = np.cos(x_radians) + 1j * np.sin(x_radians)  # BPSK complex symbols
 x_symbols = np.repeat(x_symbols, 3)  # 16 samples per symbol
 
 for k in range(16):
-    ack_packet = int_to_5bit_array(k)
+    ack_packet = int_to_5bit_array(k+1)
     ack_packet = np.where(ack_packet > 0, -1, 1)
     ack_packet = np.repeat(ack_packet, 3)
     samples = np.concatenate((start_sequence, ack_packet, x_symbols)) # FOR THE GRAPH 0 is positive, 1 is negative
@@ -58,8 +58,10 @@ for k in range(16):
     #print("DATA WITH START SEQUENCE: ",bpsk_values)
     samples = samples * 2**14  # Scale the samples for PlutoSDR
     # Now 'samples' contains the BPSK PACKET to transmit
+    sdr.tx_destroy_buffer()
     sdr.tx_cyclic_buffer = True # Enable cyclic buffers
     success = False
+    sdr.tx_destroy_buffer()
 
     sdr.tx(samples) # start transmitting
 
@@ -95,10 +97,11 @@ for k in range(16):
             if samples_after_barker < len(ack_packet)-1:
                 continue
             # Print the results
-            print(f"Updated Peak Value: {peak_value}, Peak Index in Cross-Correlation: {peak_index}")
+            #print(f"Updated Peak Value: {peak_value}, Peak Index in Cross-Correlation: {peak_index}")
         else:
             # Print the results
-            print(f" 1st Peak Value: {peak_value}, Peak Index in Cross-Correlation: {peak_index}")
+            #print(f" 1st Peak Value: {peak_value}, Peak Index in Cross-Correlation: {peak_index}")
+            pass
 
         # Plot the cross-correlation result
         # Define the lag for plotting the cross-correlation
@@ -114,13 +117,13 @@ for k in range(16):
         if peak_value>0:
             converted_array = np.where(extracted_samples.real > 0, 0, 1)
             flipped_ack_packet = np.where(ack_packet > 0, 1, 0)
-            print(converted_array)
-            print(flipped_ack_packet)
+            #print(converted_array)
+            #print(flipped_ack_packet)
         else:
             converted_array = np.where(extracted_samples.real > 0, 1, 0)
             flipped_ack_packet = np.where(ack_packet > 0, 0, 1)
-            print(converted_array)
-            print(flipped_ack_packet)
+            #print(converted_array)
+            #print(flipped_ack_packet)
         if len(converted_array) % 3 == 0:
             reshaped_array = converted_array.reshape(-1, 3)
             reshaped_array2 = flipped_ack_packet.reshape(-1, 3)
@@ -135,21 +138,22 @@ for k in range(16):
             print('100% sucess transmission')
             success = True
         else:
+            #print('wrong ack packet:',reduced_array)
             num_wrong_ack_packets +=1
             #exit()
-    sdr.tx_destroy_buffer()
-    plt.figure(0)
-    plt.plot(rx_samples,'.-')
-    lag = np.arange(-len(start_sequence) + 1, len(rx_samples))
+    
+    #plt.figure(0)
+    #plt.plot(rx_samples,'.-')
+    #lag = np.arange(-len(start_sequence) + 1, len(rx_samples))
     # Plot the cross-correlation on the same graph
     # Shift the cross-correlation by the length of the start sequence to align with received signal
-    plt.plot(rx_samples, '.', label='Received Signal', alpha=0.5)
-    plt.plot(lag + len(start_sequence) - 1, cross_corr, label='Cross-Correlation', color='r')
-    plt.title('Received Samples and Cross-Correlation')
-    plt.xlabel('Sample Index')
-    plt.ylabel('Amplitude / Correlation Value')
-    plt.axhline(0, color='grey', lw=0.5, ls='--')  # Add a horizontal line at y=0 for reference
-    plt.legend()
-    plt.grid()
+    #plt.plot(rx_samples, '.', label='Received Signal', alpha=0.5)
+    #plt.plot(lag + len(start_sequence) - 1, cross_corr, label='Cross-Correlation', color='r')
+    #plt.title('Received Samples and Cross-Correlation')
+    #plt.xlabel('Sample Index')
+    #plt.ylabel('Amplitude / Correlation Value')
+    #plt.axhline(0, color='grey', lw=0.5, ls='--')  # Add a horizontal line at y=0 for reference
+    #plt.legend()
+    #plt.grid()
     print("wrong ack packets: ",num_wrong_ack_packets)
     #plt.show()
